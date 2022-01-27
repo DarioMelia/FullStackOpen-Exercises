9b53c9aa30500f6668586ea5336edb0baeb2acd9
DarioMeliaBasus <dario@meliabasus.com>
2022-01-27 22:26:45 +0100

    unicafe step4 solved

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,61 +1,72 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-const App = ()=>{
-  const[good, setGood] = useState(0);
-  const[neutral, setNeutral] = useState(0);
-  const[bad, setBad] = useState(0);
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  
-  
   const stats = {
     good,
     neutral,
-    bad
-  }
+    bad,
+  };
 
-
-
-
-  const handleClick = (reviewType) =>{
-    switch(reviewType){
-      case "g": return () => setGood(good + 1)       
-      case "n": return () => setNeutral(neutral +1)
-      case "b": return () => setBad(bad + 1)
-      default: return;
+  const handleClick = (reviewType) => {
+    switch (reviewType) {
+      case "g":
+        return () => setGood(good + 1);
+      case "n":
+        return () => setNeutral(neutral + 1);
+      case "b":
+        return () => setBad(bad + 1);
+      default:
+        return;
     }
-  }
+  };
 
-  return(
+  return (
     <>
-    <h1>Give FeedBack</h1>
-    <Button onClick={handleClick("g")} text="good"></Button>
-    <Button onClick={handleClick("n")} text="neutral"></Button>
-    <Button onClick={handleClick("b")} text="bad"></Button>
-    <Statistics stats={stats}></Statistics>
-   
+      <h1>Give FeedBack</h1>
+      <Button onClick={handleClick("g")} text="good"></Button>
+      <Button onClick={handleClick("n")} text="neutral"></Button>
+      <Button onClick={handleClick("b")} text="bad"></Button>
+      <Statistics stats={stats}></Statistics>
     </>
-  )
-}
+  );
+};
 
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
-const Statistics = ({stats}) =>{
-  const{good,neutral,bad} = stats;
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+const Statistics = ({ stats }) => {
+  const { good, neutral, bad } = stats;
   let allFeedBack = good + neutral + bad;
   let average = getAverage();
   let positive = getPositivePer();
-  function getAverage(){return (good + (bad * -1))/allFeedBack};
-  function getPositivePer(){return (good / allFeedBack) * 100};
-  return(
-    <>
-    <h1>Statistics</h1>
-    <p>good {good}</p>
-    <p>neutral {neutral}</p>
-    <p>bad {bad}</p>
-    <p>all {allFeedBack}</p>
-    <p>average {average}</p>
-    <p>positive {positive}%</p>
-    </>
-  )
-}
+  function getAverage() {
+    return (good + bad * -1) / allFeedBack;
+  }
+  function getPositivePer() {
+    return (good / allFeedBack) * 100;
+  }
+  if (allFeedBack !== 0) {
+    return (
+      <>
+        <h1>Statistics</h1>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {allFeedBack}</p>
+        <p>average {average}</p>
+        <p>positive {positive}%</p>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h1>Statistics</h1>
+        <h3>No feedback was given</h3>
+      </>
+    );
+  }
+};
 
 export default App;
