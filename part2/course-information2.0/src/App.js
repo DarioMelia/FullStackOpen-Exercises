231050900b14d0d410ae94d84b437ba061e33d81
DarioMeliaBasus <dario@meliabasus.com>
2022-01-28 04:02:13 +0100

    solve excercises 2.1-2.5

--- part2/course-information2.0/src/App.js
+++ part2/course-information2.0/src/App.js
@@ -3,13 +3,13 @@ import React from "react";
 // %%%%% COMPONENTS %%%%%
 import Course from "./components/Course";
 import Title from "./components/Title";
-const App = ({courses}) => {
+const App = ({ courses }) => {
   return (
     <>
-    <Title text="Web Development Curriculum"/>
-    {courses.map((course) =>{
-      return <Course course={course} key={course.id}/>
-    })}
+      <Title text="Web Development Curriculum" />
+      {courses.map((course) => {
+        return <Course course={course} key={course.id} />;
+      })}
     </>
   );
 };
--- part2/course-information2.0/src/components/Content.js
+++ part2/course-information2.0/src/components/Content.js
@@ -5,16 +5,18 @@ import Total from "./Total";
 
 const Content = ({ parts }) => {
   const gnrtParts = () => {
-   return parts.map((part) => (
+    return parts.map((part) => (
       <Part key={part.id} name={part.name} exercises={part.exercises} />
     ));
   };
-  const allEx =parts.map((part)=>part.exercises).reduce((sum,cur) => sum + cur);
+  const allEx = parts
+    .map((part) => part.exercises)
+    .reduce((sum, cur) => sum + cur);
   return (
-  <>
-  <div>{gnrtParts()}</div>
-  <Total allEx={allEx}/>
-  </>
+    <>
+      <div>{gnrtParts()}</div>
+      <Total allEx={allEx} />
+    </>
   );
 };
 
