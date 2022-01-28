989a37f99a54617355edc1a292aa0228899ca0d3
DarioMeliaBasus <dario@meliabasus.com>
2022-01-28 14:47:29 +0100

    Phone book step5 solved

--- part2/phone-book/src/components/Form.js
+++ part2/phone-book/src/components/Form.js
@@ -2,13 +2,19 @@ import React from "react";
 const Form = ({onSubmit,onChange, newName, newNum})=>(
     <form onSubmit={onSubmit}>
     <div>
-      name: <input type="text" onChange={onChange} value={newName}/>
+      <Input label="name" type="text" onChange={onChange} value={newName}/>
       <br/>
-      number: <input type="number" onChange={onChange} value={newNum}/>
+      <Input label="number" type="number" onChange={onChange} value={newNum}/>
     </div>
     <div>
       <button type="submit">add</button>
     </div>
   </form>
 );
+const Input =({label,type,onChange,value})=>{
+    return <>{label}: <input type={type} onChange={onChange} value={value}/></>
+}
+
+
+
 export default Form;
--- part2/phone-book/src/components/NumberDis.js
+++ part2/phone-book/src/components/NumberDis.js
@@ -2,14 +2,16 @@ import React from "react";
 const NumbersDis = ({ persons, filter, onFilterChange }) => {
   const gnrtNums = () =>
     persons.map((person) => <NumberItm key={person.name} person={person} />);
+    
   return (
     <>
       <h2>Numbers</h2>
-      filter by name: <input value={filter} onChange={onFilterChange}/>
+      <Filter type={"name"} filter={filter} onFilterChange={onFilterChange}></Filter>
       {gnrtNums()}
     </>
   );
 };
 const NumberItm = ({ person }) => <p>{person.name} {person.number}</p>;
+const Filter = ({type, filter, onFilterChange}) => <>filter by {type}: <input value={filter} onChange={onFilterChange}/></>
 
 export default NumbersDis;
