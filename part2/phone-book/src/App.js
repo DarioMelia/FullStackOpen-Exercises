a09bc58ba83b25446a0e2dcdef72aa66d725f3f3
DarioMeliaBasus <dario@meliabasus.com>
2022-01-28 14:48:23 +0100

    Finish exercises 2.6-2.10

--- part2/phone-book/src/App.js
+++ part2/phone-book/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 // %%% COMPONENTS %%%
 import Form from "./components/Form";
@@ -6,34 +6,48 @@ import NumbersDis from "./components/NumberDis";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNum, setNewNum] = useState("");
   const [filter, setFilter] = useState("");
 
-  let numsToShow = persons.filter(num => num.name.toLowerCase().includes(filter.toLowerCase()));
-  
-  const onChange = e => e.target.type === "text"?setNewName(e.target.value):setNewNum(e.target.value);
-  const onFilterChange = e => setFilter(e.target.value);
-  const onSubmit = e =>{
+  let numsToShow = persons.filter((num) =>
+    num.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  const onChange = (e) =>
+    e.target.type === "text"
+      ? setNewName(e.target.value)
+      : setNewNum(e.target.value);
+  const onFilterChange = (e) => setFilter(e.target.value);
+  const onSubmit = (e) => {
     e.preventDefault();
     nameExists()
-      ?alert(`${newName} is already added to the phone book`)
-      :setPersons(persons.concat({name:newName,number:newNum}));
-  }
-  const nameExists = ()=> persons.find(el => el.name === newName);
-  
+      ? alert(`${newName} is already added to the phone book`)
+      : setPersons(persons.concat({ name: newName, number: newNum }));
+  };
+  const nameExists = () => persons.find((el) => el.name === newName);
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <Form onSubmit={onSubmit} onChange={onChange} newName={newName} newNum={newNum}/>
-      <NumbersDis persons={numsToShow} filter={filter} onFilterChange={onFilterChange}/>
+      <Form
+        onSubmit={onSubmit}
+        onChange={onChange}
+        newName={newName}
+        newNum={newNum}
+      />
+      <NumbersDis
+        persons={numsToShow}
+        filter={filter}
+        onFilterChange={onFilterChange}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- part2/phone-book/src/components/NumberDis.js
+++ part2/phone-book/src/components/NumberDis.js
@@ -2,16 +2,28 @@ import React from "react";
 const NumbersDis = ({ persons, filter, onFilterChange }) => {
   const gnrtNums = () =>
     persons.map((person) => <NumberItm key={person.name} person={person} />);
-    
+
   return (
     <>
       <h2>Numbers</h2>
-      <Filter type={"name"} filter={filter} onFilterChange={onFilterChange}></Filter>
+      <Filter
+        type={"name"}
+        filter={filter}
+        onFilterChange={onFilterChange}
+      ></Filter>
       {gnrtNums()}
     </>
   );
 };
-const NumberItm = ({ person }) => <p>{person.name} {person.number}</p>;
-const Filter = ({type, filter, onFilterChange}) => <>filter by {type}: <input value={filter} onChange={onFilterChange}/></>
+const NumberItm = ({ person }) => (
+  <p>
+    {person.name} {person.number}
+  </p>
+);
+const Filter = ({ type, filter, onFilterChange }) => (
+  <>
+    filter by {type}: <input value={filter} onChange={onFilterChange} />
+  </>
+);
 
 export default NumbersDis;
