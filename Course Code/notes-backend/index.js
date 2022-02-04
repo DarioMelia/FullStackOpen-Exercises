0a5edef9a444f4ecc92d8e55a344848a2451a4fa
DarioMeliaBasus <dario@meliabasus.com>
2022-02-04 12:08:44 +0100

    add middleware for not declared routes

--- Course Code/notes-backend/index.js
+++ Course Code/notes-backend/index.js
@@ -6,15 +6,19 @@ const apiRoutes = require("./api/routes.js");
 app.use(express.json())
 
 
-
-
-
 app.get("/", (req,res) => {
     res.send("<h1>Hello World</h1>")
 })
 
 
 app.use("/api", apiRoutes); 
+
+// MidleWare que al ser llamada despues de las rutas, sol oactuará para las rutas no definidas mandando 404
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  app.use(unknownEndpoint)
 
 const PORT = "3001";
 app.listen(PORT)
